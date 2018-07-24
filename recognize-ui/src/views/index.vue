50fc49b7bb77ca0de8b00e05e93722d04da6cc54
liumapp <liumapp.com@gmail.com>
2018-07-24 15:04:39 +0800

    add two steps

--- recognize-ui/src/views/index.vue
+++ recognize-ui/src/views/index.vue
@@ -6,10 +6,31 @@
  * @date 7/24/18
  */
 <template>
-
+  <div>
+    <Row>
+      <Col span="18" offset="3">
+      <Card>
+        <Steps :current="current">
+          <Step title="上传匹对图片" content="上传一张自拍照和一张身份证正面照片，匹对两张图片的人脸是否为同一个人"></Step>
+          <Step title="配对结果" content="图片相似程度，包括清晰度等参数"></Step>
+        </Steps>
+        <doc-upload v-if="current == 0" @next="nextStep" @setDocData="setDocData" :convertId="convertId"></doc-upload>
+        <converting v-else-if="current == 1" :docList="docList" @next="nextStep" :convertId="convertId"></converting>
+        <div v-else>
+          wrong current value
+        </div>
+      </Card>
+      </Col>
+    </Row>
+  </div>
 </template>
 <script>
 export default {
-  name: 'index'
+  name: 'index',
+  data () {
+    return {
+      current: 0
+    }
+  }
 }
 </script>
