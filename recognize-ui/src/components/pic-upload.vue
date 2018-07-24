5d34a5d8c7ae48235e119f02956e98dbe4dee321
liumapp <liumapp.com@gmail.com>
2018-07-24 17:07:16 +0800

    upload base64 pic

--- recognize-ui/src/components/pic-upload.vue
+++ recognize-ui/src/components/pic-upload.vue
@@ -7,11 +7,91 @@
  */
 <template>
   <div>
-    this is pic upload
+    <br>
+    <Row>
+      <Col span="8" offset="8">
+      <Upload
+        multiple
+        type="drag"
+        action="#"
+        accept=".jpg, .jpeg, .png"
+        :before-upload="handleFileToBase64"
+        :default-file-list="readyUploadPicList"
+        :on-remove="handleRmFile">
+        <div style="padding: 20px 0">
+          <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
+          <p>Click or drag files here to upload</p>
+        </div>
+      </Upload>
+      </Col>
+    </Row>
+    <Row>
+      <Col span="2" offset="14">
+      <Button type="primary" @click="submitPic">提交</Button>
+      </Col>
+    </Row>
   </div>
 </template>
 <script>
 export default {
-  name: 'pic-upload'
+  name: 'pic-upload',
+  data () {
+    return {
+      picList: [],
+      readyUploadPicList: []
+    }
+  },
+  methods: {
+    handleFileToBase64 (file) {
+      let reader = new FileReader();
+      let _vue = this;
+      reader.readAsDataURL(file);
+      reader.onload = function () {
+        _vue.fileList.push({name: file.name, content: reader.result, convertId: _vue.cid});
+        _vue.readyUploadFile.push({name: file.name});
+      }
+      return false;
+    },
+    handleRmFile (file) {
+      console.log(file);
+      let _vue = this;
+      let i = 0;
+      this.fileList.forEach(function (e) {
+        if (e.name == file.name) {
+          _vue.fileList.splice(i,1);
+          _vue.readyUploadFile.splice(i,1);
+        }
+        i++;
+      });
+    },
+    submitPic () {
+      if (!this.isEmpty()) {
+        util.post('upload/multybase64', this.fileList).then(res => {
+          this.$Message.success('file upload success!');
+          this.$emit('setDocData',this.handleDocList());
+          this.$emit('next');
+        });
+      } else {
+        this.$Message.error('请至少上传一个doc文件');
+      }
+    },
+    isEmpty () {
+      return this.fileList.length == 0;
+    },
+    /**
+     * 0: waiting convert ; 1: converting success
+     */
+    handleDocList () {
+      let docList = [];
+      this.fileList.forEach(function (e) {
+        docList.push({
+          filename: e.name,
+          convertId: e.convertId,
+          status: 0
+        })
+      });
+      return docList;
+    }
+  }
 }
 </script>
