f70cc4af0097c26399e72e1943774e56bc8d593d
liumapp <liumapp.com@gmail.com>
2018-07-24 17:14:28 +0800

    add main class

---
+++ recognize-service/src/main/java/com/liumapp/recognize/service/RecognizeServiceMain.java
@@ -0,0 +1,20 @@
+package com.liumapp.recognize.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liumapp
+ * @file RecognizeServiceMain.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/24/18
+ */
+@SpringBootApplication
+public class RecognizeServiceMain {
+
+    public static void main (String[] args) {
+        SpringApplication.run(RecognizeServiceMain.class, args);
+    }
+
+}
