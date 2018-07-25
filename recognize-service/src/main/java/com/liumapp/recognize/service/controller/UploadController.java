aa9c005646defd228b2ca79c1b2df8c738c300ab
liumapp <liumapp.com@gmail.com>
2018-07-25 09:52:25 +0800

    save pic in local

--- recognize-service/src/main/java/com/liumapp/recognize/service/controller/UploadController.java
+++ recognize-service/src/main/java/com/liumapp/recognize/service/controller/UploadController.java
@@ -2,6 +2,8 @@ package com.liumapp.recognize.service.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.liumapp.recognize.service.entity.MultyDocEntity;
+import com.liumapp.recognize.service.util.FileManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,23 +23,16 @@ import java.io.IOException;
 @RequestMapping("upload")
 public class UploadController {
 
+    @Autowired
+    private FileManager fileManager;
 
     @RequestMapping("/multybase64")
     @ResponseBody
     public String multyBase64Upload (@RequestBody MultyDocEntity[] list) {
         try {
-            int index = 0;
             for (MultyDocEntity doc : list) {
                 MultipartFile file = fileManager.base64toMultipart(doc.getContent());
                 fileManager.save(file);
-                convertDocPattern.setFileIndex(index);
-                convertDocPattern.setConvertId(doc.getConvertId());
-                convertDocPattern.setDocPath(fileManager.getSavePath());
-                convertDocPattern.setPdfPath(fileManager.getSavePath());
-                convertDocPattern.setOriginalName(fileManager.getFileName());
-                convertDocPattern.setSaveName(fileManager.getFileName() + ".pdf");
-                convertDocPublisher.send(JSON.toJSONString(convertDocPattern));
-                index++;
             }
         } catch (IOException e) {
             e.printStackTrace();
