7b37afc4766e201eeabefd077aebb1807dfde8ad
liumapp <liumapp.com@gmail.com>
2018-07-25 09:40:45 +0800

    add upload controller

---
+++ recognize-service/src/main/java/com/liumapp/recognize/service/controller/UploadController.java
@@ -0,0 +1,19 @@
+package com.liumapp.recognize.service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liumapp
+ * @file UploadController.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/24/18
+ */
+@RestController()
+@RequestMapping("upload")
+public class UploadController {
+
+
+
+}
