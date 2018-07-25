7845277bbe7748c878341f7fcc4ce28c2a871f57
liumapp <liumapp.com@gmail.com>
2018-07-25 09:50:36 +0800

    add multy doc uplaod entity

--- recognize-service/src/main/java/com/liumapp/recognize/service/controller/UploadController.java
+++ recognize-service/src/main/java/com/liumapp/recognize/service/controller/UploadController.java
@@ -1,7 +1,14 @@
 package com.liumapp.recognize.service.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.liumapp.recognize.service.entity.MultyDocEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 /**
  * @author liumapp
@@ -15,5 +22,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class UploadController {
 
 
+    @RequestMapping("/multybase64")
+    @ResponseBody
+    public String multyBase64Upload (@RequestBody MultyDocEntity[] list) {
+        try {
+            int index = 0;
+            for (MultyDocEntity doc : list) {
+                MultipartFile file = fileManager.base64toMultipart(doc.getContent());
+                fileManager.save(file);
+                convertDocPattern.setFileIndex(index);
+                convertDocPattern.setConvertId(doc.getConvertId());
+                convertDocPattern.setDocPath(fileManager.getSavePath());
+                convertDocPattern.setPdfPath(fileManager.getSavePath());
+                convertDocPattern.setOriginalName(fileManager.getFileName());
+                convertDocPattern.setSaveName(fileManager.getFileName() + ".pdf");
+                convertDocPublisher.send(JSON.toJSONString(convertDocPattern));
+                index++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return JSON.toJSONString("error");
+        }
+        return JSON.toJSONString("success");
+    }
 
 }
---
+++ recognize-service/src/main/java/com/liumapp/recognize/service/entity/MultyDocEntity.java
@@ -0,0 +1,55 @@
+package com.liumapp.recognize.service.entity;
+
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+/**
+ * @author liumapp
+ * @file MultyDocEntity.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/25/18
+ */
+@Component
+public class MultyDocEntity implements Serializable {
+
+    private String name;
+
+    private String content;
+
+    private Integer convertId;
+
+    public MultyDocEntity() {
+    }
+
+    public MultyDocEntity(String name, String content, Integer convertId) {
+        this.name = name;
+        this.content = content;
+        this.convertId = convertId;
+    }
+
+    public Integer getConvertId() {
+        return convertId;
+    }
+
+    public void setConvertId(Integer convertId) {
+        this.convertId = convertId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
