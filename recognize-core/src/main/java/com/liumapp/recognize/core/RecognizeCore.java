647862cddab0ef8c558031bff3ba7061ffea993b
liumapp <liumapp.com@gmail.com>
2018-07-26 17:23:02 +0800

    add recognize core main class and jobdata and jobdetail

---
+++ recognize-core/src/main/java/com/liumapp/recognize/core/RecognizeCore.java
@@ -0,0 +1,14 @@
+package com.liumapp.recognize.core;
+
+/**
+ * @author liumapp
+ * @file RecognizeCore.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/7/26
+ */
+public class RecognizeCore {
+
+
+
+}
---
+++ recognize-core/src/main/java/com/liumapp/recognize/core/job/JobDetail.java
@@ -0,0 +1,14 @@
+package com.liumapp.recognize.core.job;
+
+/**
+ * @author liumapp
+ * @file JobDetail.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/7/26
+ */
+public abstract class JobDetail<T extends JobData> {
+
+
+
+}
