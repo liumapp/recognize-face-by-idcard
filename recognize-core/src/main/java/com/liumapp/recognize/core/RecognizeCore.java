63f4e8c689258701107a9b7fbed3de92720ee5b7
liumapp <liumapp.com@gmail.com>
2018-07-27 09:10:02 +0800

    rm job package

--- recognize-core/src/main/java/com/liumapp/recognize/core/RecognizeCore.java
+++ recognize-core/src/main/java/com/liumapp/recognize/core/RecognizeCore.java
@@ -1,5 +1,9 @@
 package com.liumapp.recognize.core;
 
+import com.liumapp.recognize.core.config.BaiduApiConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
 /**
  * @author liumapp
  * @file RecognizeCore.java
@@ -7,8 +11,8 @@ package com.liumapp.recognize.core;
  * @homepage http://www.liumapp.com
  * @date 2018/7/26
  */
+@Configuration
+@Import({BaiduApiConfig.class})
 public class RecognizeCore {
-
-
 
 }
